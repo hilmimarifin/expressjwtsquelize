c40b719b72ae7428daff777e76f5ea1ca9efbccd
hilmimarifin <hilmi.m.arifin@gmail.com>
2021-03-20 21:26:26 +0700

    initial commit

---
+++ model/user.model.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define('users', {
+      lastname: {
+        type: Sequelize.STRING
+      },
+      firstname: {
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      }
+    });
+    
+    return User;
+  }
